aa3419bd82e0dc65a67fb04666612a7f3e00dd04
Aku Silvennoinen <augustus58@gmail.com>
2015-05-17 10:21:35 +0300

    added info button to ui and changed colors in ui buttons

---
+++ cwa/app/assets/javascripts/ui.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('[data-toggle="popover"]').popover({
+	html : true,
+	content: function() {
+	    return $('#popover_content_wrapper').html();
+	}
+    })
+})
+
+$( "#target" ).submit(function( event ) {
+    alert( "Handler for .submit() called." );
+    event.preventDefault();
+});
