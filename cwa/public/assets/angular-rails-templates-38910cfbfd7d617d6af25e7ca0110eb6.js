a59e18533003845df869d60905c047e9592e89e8
Aku Silvennoinen <augustus58@gmail.com>
2015-05-26 22:26:10 +0300

    assets deleted

--- cwa/public/assets/angular-rails-templates-38910cfbfd7d617d6af25e7ca0110eb6.js
+++
@@ -1,8 +0,0 @@
-// Angular Rails Templates 0.2.0
-//
-// angular_templates.ignore_prefix: ["templates/"]
-// angular_templates.markups: ["erb", "str"]
-// angular_templates.htmlcompressor: false
-
-angular.module("templates", []);
-
