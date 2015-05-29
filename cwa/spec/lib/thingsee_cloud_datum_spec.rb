da49b409d9b4f07f6f28b94473d111daf1f40b83
viipu <virpi.sumu@gmail.com>
2015-05-29 13:40:55 +0300

    fixed broken ts test

--- cwa/spec/lib/thingsee_cloud_datum_spec.rb
+++ cwa/spec/lib/thingsee_cloud_datum_spec.rb
@@ -3,20 +3,37 @@ require 'rspec/rails'
 describe 'ThingseeCloudDatum' do
 
   it 'should parse and return a single temperature record returned through get' do
-    canned_answer = File.new("./spec/lib/samples/thingsee_single_temp.json").read
+    canned_answer = File.new("./spec/lib/samples/thingsee_temp_with_loc.json").read
 
     #might have to specify :get regex further?
     stub_request(:get, /.*0x00060100.*/).to_return(body: canned_answer, headers: { 'Content-Type' => "application/json" })
 
     record = ThingseeCloudDatum.deliver("temperature")
     # check first station's record
-    expect(record[2]).to eq(1432850409189)
+    expect(record[2]).to eq(1432852761910)
     expect(record[3]).to eq(26.94)
     expect(record[4]).to eq("thingsee")
 
   end
 
-  it 'should parse and return a temperature with location record returned through get' do
+  it 'should parse and return a temperature with location record from a well-formed response returned through get' do
+    canned_answer = File.new("./spec/lib/samples/thingsee_temp_with_loc.json").read
+
+    #might have to specify :get regex further?
+    stub_request(:get, /.*0x00060100.*/).to_return(body: canned_answer, headers: { 'Content-Type' => "application/json" })
+
+    record = ThingseeCloudDatum.deliver("temperature")
+    # check first station's record
+    expect(record[0]).to eq(60.1820)
+    expect(record[1]).to eq(24.9255)
+    expect(record[2]).to eq(1432852761910)
+    expect(record[3]).to eq(26.94)
+    expect(record[4]).to eq("thingsee")
+
+  end
+
+  it 'should parse and return a temperature with location record from a complex response returned through get' do
+    #TODO; wrong stub file
     canned_answer = File.new("./spec/lib/samples/thingsee_temp_with_loc.json").read
 
     #might have to specify :get regex further?
