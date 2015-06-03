22a8df171263242dd742704522d6e9b6d73f90bb
Aku Silvennoinen <augustus58@gmail.com>
2015-06-03 15:14:11 +0300

    ActiveRecord::Base.logger = Logger.new(STDOUT) added to environment.rb

--- cwa/config/environment.rb
+++ cwa/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 # Initialize the Rails application.
 Rails.application.initialize!
