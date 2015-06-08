06b38efa4865876e7ee54cfff69f6b96ff00f981
Aku Silvennoinen <augustus58@gmail.com>
2015-06-08 13:27:19 +0300

    angular-rails-templates removed

--- cwa/app/assets/javascripts/application.js
+++ cwa/app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
-//= require angular-rails-templates
 //= require angular-route/angular-route
 
 //= require bootstrap-sprockets
