7cfa587d8630f9c602fb1c774c1b25821452adee
Ari Vuolas <ari.vuolas@cs.helsinki.fi>
2015-06-08 10:25:37 +0300

    deleted duplicate line from application.rb

--- cwa/config/application.rb
+++ cwa/config/application.rb
@@ -29,7 +29,7 @@ module Cwa
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :resque
 
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","fonts")
 
     #config.assets.precompile = [/\A[^\/\\]+\.(css|js)$/i]
 
