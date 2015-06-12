212a5aa2898c3c615ed2f5022d81a49f7a9dbea0
Heikki Rantala <heikki.j.rantala@helsinki.fi>
2015-06-12 12:39:11 +0300

    fixed bug on printing with Firefox

--- cwa/app/assets/javascripts/libs/printDiv.js
+++ cwa/app/assets/javascripts/libs/printDiv.js
@@ -1,6 +1,6 @@
 function printDiv(divName) {
     var printContents = document.getElementById(divName).innerHTML;
-    var popupWin = window.open('', '_blank', 'width=device-width,height=device-height');
+    var popupWin = window.open('', '_blank',  'width=auto,height=auto,scrollbars=1');
     popupWin.document.open()
     popupWin.document.write('<html></head><body onload="window.print()">' + printContents + '</html>');
     popupWin.document.close();
