03fded70c40d6ebcba2494f2b3100caddd444ee7
viipu <virpi.sumu@gmail.com>
2015-06-17 16:51:33 +0300

    basic signin functionality

--- cwa/db/schema.rb
+++ cwa/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612130230) do
+ActiveRecord::Schema.define(version: 20150617074725) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "device_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "datatransformer"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
