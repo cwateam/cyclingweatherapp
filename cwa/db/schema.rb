fe04df4de2b219d65567f43a7803e554e8f515ac
Aku Silvennoinen <augustus58@gmail.com>
2015-06-18 13:19:41 +0300

    firebase client test hopefully fixed

--- cwa/db/schema.rb
+++ cwa/db/schema.rb
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150622165030) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "color_drops", force: :cascade do |t|
+    t.integer  "value"
+    t.string   "color"
+    t.integer  "layer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "device_profiles", force: :cascade do |t|
     t.string   "data_transformer"
     t.datetime "created_at",       null: false
@@ -46,6 +54,13 @@ ActiveRecord::Schema.define(version: 20150622165030) do
     t.integer  "sensor_type_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "layers", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "layers_sensors_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "sensor_types", force: :cascade do |t|
