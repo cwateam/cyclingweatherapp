af2a516c8cf195692520e311818cc3f3786c3c9b
Heikki Rantala <heikki.j.rantala@helsinki.fi>
2015-06-18 14:28:23 +0300

    minor changes to maplegend

--- cwa/app/assets/javascripts/libs/mapLegend.js
+++ cwa/app/assets/javascripts/libs/mapLegend.js
@@ -3,12 +3,18 @@ function mapLegend(canvas) {
     var ctx = canvas.getContext("2d");
   //  ctx.fillRect(10, 10, 10, 10
 
-    ctx.rect(20,20,150,100);
-    ctx.fillStyle="white";
-    ctx.fill();
+    var canvasHeight=350;
 
+    if (screen.height<400) {
+        canvasHeight = screen.height * 0.6;
+        canvas.height = canvasHeight;
+    }
 
-    var my_gradient=ctx.createLinearGradient(0,0,0,300);
+    ctx.font="15px Georgia";
+    ctx.fillText("0",0,20);
+    ctx.fillText("20",0,120);
+
+    var my_gradient=ctx.createLinearGradient(0,0,0,canvasHeight-20);
     my_gradient.addColorStop(0,"red");
     my_gradient.addColorStop(0.2,"orange");
     my_gradient.addColorStop(0.4,"yellow");
@@ -16,5 +22,5 @@ function mapLegend(canvas) {
     my_gradient.addColorStop(0.8,"blue");
     my_gradient.addColorStop(1,"purple");
     ctx.fillStyle=my_gradient;
-    ctx.fillRect(20,20,100,300);
+    ctx.fillRect(20,20,100,canvasHeight-20);
 }
