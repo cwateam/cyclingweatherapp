210355d6368aa623cef2da282b35bc305bc98b37
Ari Vuolas <ari.vuolas@cs.helsinki.fi>
2015-06-22 11:50:11 +0300

    renamed map_service to js.erb modified gdaljob

--- cwa/app/jobs/gdal_job.rb
+++ cwa/app/jobs/gdal_job.rb
@@ -7,7 +7,7 @@ class GdalJob < ActiveJob::Base
 
     if dataType
 
-      system("cd ~/.gdal/#{dataType} && gdal_grid -zfield \"Data\" -a invdist:power=8:smoothing=0.3:nodata=0:radius1=0.1:radius2=0.1 -txe 24.40 25.40 -tye 60.700 59.700 -outsize 2000 2000 -of GTiff -ot Float64 -l dem dem.vrt dem.tiff --config GDAL_NUM_THREADS ALL_CPUS && gdaldem color-relief -alpha  dem.tiff colors.txt dem.tiff && gdal_translate -of PNG dem.tiff data.png")
+      system("cd ~/.gdal/#{dataType} && gdal_grid -zfield \"Data\" -a invdist:power=8:smoothing=0.3:nodata=0:radius1=0.1:radius2=0.1 -txe 24.40 25.40 -tye 60.700 59.700 -outsize 2000 2000 -of GTiff -ot Float64 -l dem dem.vrt dem.tiff --config GDAL_NUM_THREADS ALL_CPUS && gdaldem color-relief -alpha  dem.tiff colors.txt dem.tiff && gdal_translate -of PNG dem.tiff #{Rails.root.join('app','assets', 'images', 'data.png')} ")
 
     end
 
--- cwa/db/seeds.rb
+++ cwa/db/seeds.rb
@@ -135,10 +135,10 @@ st38.sensors.create device_profile_id:dp.id, name:"ThingseeOne ambient light sen
 layer=Layer.create sensor_type_id:st1.id, name:st1.name+" reds"
 #Create color drops for layer
 layer.color_drops.create value:0, color:"255,255,255,0"
-layer.color_drops.create value:5, color:"255,204,204,0"
-layer.color_drops.create value:10, color:"255,153,153,0"
-layer.color_drops.create value:15, color:"255,102,102,0"
-layer.color_drops.create value:20, color:"255,51,51,0"
-layer.color_drops.create value:25, color:"255,0,0,0"
-layer.color_drops.create value:30, color:"204,0,0,0"
+layer.color_drops.create value:5, color:"255,204,204"
+layer.color_drops.create value:10, color:"255,153,153"
+layer.color_drops.create value:15, color:"255,102,102"
+layer.color_drops.create value:20, color:"255,51,51"
+layer.color_drops.create value:25, color:"255,0,0"
+layer.color_drops.create value:30, color:"204,0,0"
 
