eb0e7f5880821f7ee90605969fa57174c2276b2d
Heikki Rantala <heikki.j.rantala@helsinki.fi>
2015-06-25 12:25:40 +0300

    Some refactoring and commenting on front

--- cwa/app/assets/javascripts/libs/printDiv.js
+++ cwa/app/assets/javascripts/libs/printDiv.js
@@ -1,3 +1,7 @@
+// Function opens a new window that has only the content of given html div
+// and tries to open a print window for that content.
+// Used here to print the directions.
+
 function printDiv(divName) {
     var printContents = document.getElementById(divName).innerHTML;
     var popupWin = window.open('', '_blank',  'width=auto,height=auto,scrollbars=1');
